6604b3c9f3a4c3b9da21be199f8938953a000a5c
Nish Tahir <nishtahir@outlook.com>
2016-05-13 19:18:10 +0300

    Fixed code programs after failure

--- avr-compile-prog.sh
+++ avr-compile-prog.sh
@@ -1,10 +1,15 @@
-
 #!/bin/bash
+set -o errexit
+
 c_file=$1
 file_path="${c_file%.*}"
 
 echo "Compiling code..."
-avr-gcc -mmcu=atmega88 -Os $c_file -o $file_path.elf
+avr-gcc -mmcu=atmega88 -Os $c_file -o $file_path.elf || {
+    echo "Compilation failed"; exit 1;
+}
 
 echo "Uploading..."
-avrdude -p m88p -P usb -c usbasp -Uflash:w:$file_path.elf
+avrdude -p m88p -P usb -c usbasp -Uflash:w:$file_path.elf || {
+    echo "Programming failed"; exit 1;
+}

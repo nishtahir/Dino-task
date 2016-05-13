ef2150da9da239c9360822392de6d36fce648907
Nish Tahir <nishtahir@outlook.com>
2016-05-13 19:11:15 +0300

    Can now select dino from screen

--- t-rex.c
+++ t-rex.c
@@ -7,6 +7,7 @@
 #define NR_WORDS 11
 #define LCD_NEW_LINE	0xC0 // New LCD line
 #define LCD_CLEAR	0x01 // Clear the LCD screen
+#define LCD_CARRIAGE_RETURN 0x10 // Go back to begining of line
 
 #include <avr/io.h>                 //Standard include for ATMega16
 #include <util/delay.h>
@@ -23,7 +24,7 @@ int obstacleGenerator();
 void die();
 
 /*Global Variable Declarations*/
-int ADCValue; 
+int ADCValue;
 
 /*
  * The main program
@@ -31,43 +32,75 @@ int ADCValue;
 int main() {
     char dino;
     int game_status;
-    
+
     /* Initializations */
     LCD_init();
-    
+
+    // Setup port C
+    DDRC = 0xE4;
+    PORTC = 0xFF;
+
     /* Set up the ADC */
     //AVcc as reference voltage, justify the bits of the analog input to the right and choos ADC4 as the analog input
     ADMUX=(1<<REFS0)|(0<<ADLAR) | (1<<MUX2);
-    
+
     //Enable ADC, enable interrupts for ADC, prescaler = 128
     ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
-    
+
     //Disable the digital input on ADC4, just to be sure
     DIDR0 = (1 << ADC4D);
-    
+
+    sei();
+
+    ADCSRA |= (1 << ADSC); //
     /* Actual program starts here */
+    while(1){
+        LCD_command(LCD_CLEAR); // clear lcd
+        _delay_ms(5);
+        LCD_write("Select Your Dino");
+        LCD_command(LCD_NEW_LINE); //Change the line
+        LCD_write("Press PC0");
+
+        while((PINC & (1 << PC0)) == (1 << PC0));
+        charSelection();
+        // LCD_command(LCD_CLEAR);
+        // _delay_ms(5);
+        // LCD_write("Do you feel");
+        // LCD_command(LCD_NEW_LINE); //Change the line
+        // LCD_write("in charge?");
+        _delay_ms(500);
+    }
 
     return 0;
 }
 
 /*
  * Select a dino to play with.
- * 
+ *
  * Return: The chosen dino character
  */
 char charSelection() {
-    
+    char dino;
+    char dino_family[] = {'f', 'q', 0x5C, 0xFC, 0xE6};
+
     LCD_command(LCD_CLEAR);
     _delay_ms(5);
     LCD_write("Select Your Dino");
     LCD_command(LCD_NEW_LINE); //Change the line
-    
-} 
+
+    while(1){
+        dino = dino_family[ADCValue];
+        LCD_putchar(dino);
+        LCD_command(LCD_CARRIAGE_RETURN); //Change the line
+    }
+
+    return dino;
+}
 
 /* The dino game
- * 
+ *
  * char dino: the dino character we are going to play with
- * 
+ *
  * return: 1 when we die
  */
 int game(char dino) {
@@ -83,13 +116,13 @@ void die(){
     LCD_write("R.I.P. Dino! :(");
     LCD_command(LCD_NEW_LINE); //Change the line
     LCD_write("PC3 - Next Life");
-    
-    
+
+
     // Dying song
     PORTB |= (1 << PB6) | (1 << PB0);
     _delay_ms(500);
     PORTB &= (0 << PB6) & (1 << PB0);
-    
+
     while ((PINC & (1 << PC3)) == (1 << PC3));
     return;
 }
@@ -99,10 +132,10 @@ void die(){
  */
 //Catch the interrupts of the ADC
 ISR(ADC_vect){
-        
+
     //Let's find the value of the ADC (no need to calculate the voltage)
-    ADCValue = (ADCL + (ADCH<<8)); 
-    
+    ADCValue = (ADCL + (ADCH<<8));
+
     //Assing a character in the English alphabet to the value of the ADC
     ADCValue = (ADCValue * 5)/1024;
     _delay_us(50);
@@ -110,4 +143,3 @@ ISR(ADC_vect){
     //Let's start the conversion again (as it was automatically disabled whet the interrupt fired)
     ADCSRA |= (1 << ADSC);
 }
-
